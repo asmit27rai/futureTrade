17e7cce333d359bc7df5820b048ff97e4ed43150
asmit27rai <raiasmit10@gmail.com>
2024-10-27 19:15:27 +0530

    Added Order Book

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,21 @@ import Graph from "./components/Graph";
 import BuySell from "./components/BuySell";
 import Header from './components/Header';
 import History from './components/History';
+import Orderbook from './components/OrderBook';
 
 function App() {
   return (
     <div className="flex flex-col w-full bg-black text-white">
       <Navbar />
       <Header />
-      <div className="flex flex-grow h-[calc(100vh-64px)] space-x-8 p-8 overflow-y-auto">
-        <div className="w-2/3">
+      <div className="flex flex-grow h-[calc(100vh-64px)] space-x-4 p-4 overflow-y-auto">
+        <div className="w-3/5">
           <Graph />
         </div>
-        <div className="w-1/3">
+        <div className="w-1/5">
+          <Orderbook />
+        </div>
+        <div className="w-1/5">
           <BuySell />
         </div>
       </div>
